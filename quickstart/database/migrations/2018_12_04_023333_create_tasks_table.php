9776b47d236f3b9010b00d460b6cdb4b08d14df2
duyle1996 <duylebkdn@gmail.com>
2018-12-04 09:38:00 +0700

    create migrations

--- quickstart/database/migrations/2018_12_04_023333_create_tasks_table.php
+++ quickstart/database/migrations/2018_12_04_023333_create_tasks_table.php
@@ -16,7 +16,7 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email');
+            $table->string('email')->unique();
             $table->string('password');
             $table->timestamps();
         });
